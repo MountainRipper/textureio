a0f867a53e4c02ba5e4dc1239b3e4518ff2afc0e
xuwei <xuwei17385@153.com>
2023-07-02 19:52:52 +0800

    use prebuild stb/imgui bundle

--- test/example_3rd_bundle.cpp
+++
@@ -1,8 +0,0 @@
-#define MR_SDL_RUNNER_IMPLEMENT
-#include "mrcommon/sdl_runner.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_image_write.h>
